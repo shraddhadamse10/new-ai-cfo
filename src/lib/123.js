20e9eceedf5447044d492bf541473b2763c26782
paw859 <pawfectfinds203@gmail.com>
2025-06-05 16:43:04 +0530

    Update 123.js

--- src/lib/123.js
+++ src/lib/123.js
@@ -1,13 +1,1 @@
-useEffect(() => {
-  const testSupabase = async () => {
-    try {
-      console.log('Testing Supabase connection...');
-      const { data, error } = await supabase.auth.getSession();
-      console.log('Supabase test result:', { data, error });
-    } catch (err) {
-      console.error('Supabase connection failed:', err);
-    }
-  };
-  
-  testSupabase();
-}, []);
+
